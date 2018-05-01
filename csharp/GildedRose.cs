38d885152c3e1fd1e995c7585a73028847c9dbab
Matúš Bezek <matusbzk@gmail.com>
2018-05-01 18:02:38 +0200

    Presun updatovacej logiky do vlastnej statickej triedy

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,90 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using csharp.QualityUpdaters;
 
 namespace csharp
 {
     public class GildedRose
     {
+        // parameters
         IList<Item> Items;
 
+        // constructors
+        #region GildedRose(IList<Item> Items)
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
         }
+        #endregion
 
-        public Item UpdateQuality(Item item)
-        {
-            if (item.Name.ToLower().Contains("sulfuras"))
-            {
-                return item;
-            }
 
-            item.SellIn--;
-
-            int qualityDifference = item.Quality > 0 && item.Quality < 50 ? -1 : 0;
-
-            if (item.Name.ToLower().Contains("aged brie") || item.Name.ToLower().Contains("backstage passes"))
-            {
-                qualityDifference *= -1;
-            }
-
-            if (item.Name.ToLower().Contains("backstage passes"))
-            {
-                if (item.SellIn <= 0)
-                {
-                    item.Quality = 0;
-                    return item;
-                }
-
-                if (item.SellIn < 11)
-                {
-                    qualityDifference += 1;
-                }
-
-                if (item.SellIn < 6)
-                {
-                    qualityDifference += 1;
-                }
-            }
-
-            item.Quality += qualityDifference;
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != "Aged Brie")
-                {
-                    if (!item.Name.ToLower().Contains("backstagepasses"))
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = 0;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
-
-            return item;
-        }
-
+        // public methods
+        #region UpdateQuality()
         public void UpdateQuality()
         {
             this.Items = this.Items
-                .Select(this.UpdateQuality)
+                .Select(BasicQualityUpdater.UpdateQuality)
                 .ToList();
         }
+        #endregion
     }
 }
