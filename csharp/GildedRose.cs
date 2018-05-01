258dfe5e0bb126fbc92166e87ac0206f7811108b
Matúš Bezek <matusbzk@gmail.com>
2018-05-01 15:27:29 +0200

    Prvý veľký refactoring metódy UpdateQuality + nové testy -prípady okolo 50 a 0, začiatok backstage passes

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -6,6 +6,7 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -20,48 +21,33 @@ namespace csharp
 
             item.SellIn--;
 
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            int qualityDifference = item.Quality > 0 && item.Quality < 50 ? -1 : 0;
+
+            if (item.Name.ToLower().Contains("aged brie") || item.Name.ToLower().Contains("backstage passes"))
             {
-                if (item.Quality > 0)
+                qualityDifference *= -1;
+            }
+
+            if (item.Name.ToLower().Contains("backstage passes"))
+            {
+                if (item.SellIn < 11)
                 {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
+                    qualityDifference += 1;
+                }
+
+                if (item.SellIn < 6)
+                {
+                    qualityDifference += 1;
                 }
             }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
 
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            item.Quality += qualityDifference;
 
             if (item.SellIn < 0)
             {
                 if (item.Name != "Aged Brie")
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (!item.Name.ToLower().Contains("backstagepasses"))
                     {
                         if (item.Quality > 0)
                         {
--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -37,6 +37,36 @@ namespace csharp
         }
 
         [Test]
+        public void QualityGoingBelowZeroBeforeSellInTest()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Frozen Pizza: Quattro Stagioni", SellIn = 42, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+            Assert.AreEqual(41, Items[0].SellIn);
+        }
+
+        [Test]
+        public void QualityGoingBelowZeroAfterSellInFrom0Test()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Frozen Pizza: Quattro Stagioni", SellIn = -15, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+            Assert.AreEqual(-16, Items[0].SellIn);
+        }
+
+        [Test]
+        public void QualityGoingBelowZeroAfterSellInFrom1Test()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Frozen Pizza: Quattro Stagioni", SellIn = -15, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+            Assert.AreEqual(-16, Items[0].SellIn);
+        }
+
+        [Test]
         public void AgedBrieBeforeSellInTest()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 7 } };
@@ -44,6 +74,46 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual(8, Items[0].Quality);
             Assert.AreEqual(4, Items[0].SellIn);
+        }
+
+        [Test]
+        public void AgedBrieAfterSellInTest()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -5, Quality = 7 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, Items[0].Quality);
+            Assert.AreEqual(-6, Items[0].SellIn);
+        }
+
+        [Test]
+        public void AgedBrieGoingOver50BeforeSellInTest()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 15, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+            Assert.AreEqual(14, Items[0].SellIn);
+        }
+
+        [Test]
+        public void AgedBrieGoingOver50AfterSellInFrom50Test()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -15, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+            Assert.AreEqual(-16, Items[0].SellIn);
+        }
+
+        [Test]
+        public void AgedBrieGoingOver50AfterSellInFrom49Test()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -15, Quality = 49 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+            Assert.AreEqual(-16, Items[0].SellIn);
         }
 
         [Test]
@@ -75,5 +145,16 @@ namespace csharp
             Assert.AreEqual(80, Items[0].Quality);
             Assert.AreEqual(-5, Items[0].SellIn);
         }
+
+        [Test]
+        public void BackstagePassesLongBeforeSellInTest()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to Apocalyptica concert", SellIn = 25, Quality = 12 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(13, Items[0].Quality);
+            Assert.AreEqual(24, Items[0].SellIn);
+        }
+
     }
 }
